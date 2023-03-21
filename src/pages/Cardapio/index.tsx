8f8fe58deedd9245b3ee0ecbeab05d99882a129e
Rander Felipe Miranda de Paula <rander.paula@estudante.iftm.edu.br>
2023-03-21 14:15:24 -0300

    Colocando logo e primeiras estilos

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,12 @@
+import React from "react";
 import styles from "./Cardapio.module.scss";
-
+import {ReactComponent as Logo} from 'assets/logo.svg';
 
 export default function Cardapio(){
-    return (
+    return(
         <main>
             <nav className={styles.menu}>
-                Aluroni
+                <Logo/>
             </nav>
         </main>
     )
