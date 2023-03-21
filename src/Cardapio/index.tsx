420617053a0fcd37467bc54d21984b432a39c43f
Rander Felipe Miranda de Paula <rander.paula@estudante.iftm.edu.br>
2023-03-21 11:21:00 -0300

    Segundo commit

---
+++ src/Cardapio/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./Cardapio.module.scss";
+
+
+export default function Cardapio(){
+    return (
+        <main>
+            <nav className={styles.menu}>
+                Aluroni
+            </nav>
+        </main>
+    )
+}
