0bd2acdca4fb28c67dc59ba24f68435c029b9ee3
Rander Felipe Miranda de Paula <rander.paula@estudante.iftm.edu.br>
2023-03-22 08:33:21 -0300

    Aperfeiçoando os botoes do ordenador

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,15 +1,56 @@
 import styles from './Ordenador.module.scss';
 import opcoes from './opcoes.json';
+import { useState } from 'react';
+import classNames from 'classnames';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp} from 'react-icons/md';
 
-export default function Ordenador(){
+
+interface Props{
+    ordenador: string,
+    setOrdenador : React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Ordenador({ordenador, setOrdenador}:Props){
+    //pelo principio da responsabilidade única : 
+    const [aberto, setAberto] = useState(false);
+    
+    //Para poder puxar o nome dentro do array do json, deve se usar:
+    const nomeOrdenador = ordenador && opcoes.find(opcao=> opcao.value === ordenador)?.nome;
+    //Assim, quando fomos mostrá-lo no botão, teremos a string e não o value
+
     return(
-        <button className={styles.ordenador}>
-            <span>Ordenar por </span>
-            <div className={styles.ordenador__options}>
+        <button className={classNames({
+            //Mostrar que o ordenador está ativo ou não
+            [styles.ordenador]: true,
+            [styles["ordenador--ativo"]] : ordenador !== "",
+        })}
+            //clickando no botão de ordenar, ele abre suas opcoes
+            onClick={()=>
+            setAberto(!aberto)}
+
+            //clickando fora, a opção é desmarcada, fechando o ordenador
+            onBlur={()=> setAberto(false)}>
+            
+            
+            <span>{ //Quando escolhe uma opcao, ela fica selecionada,
+            //para mostrar qual o tipo de ordem a ser usada
+            nomeOrdenador || "Ordenar por "}</span>
+            
+            
+            { //Setinha de cima e baixo do ordenador : 
+            aberto ? <MdKeyboardArrowUp size={20}/>
+             : < MdKeyboardArrowDown size={20}/> }
+
+            <div className={classNames({
+                [styles.ordenador__options] : true,
+                [styles["ordenador__options--ativo"]] : aberto,
+            })}
+            >
                 {opcoes.map(
                     opcao => (
                         <div className={styles.ordenador__option}
-                        key = {opcao.value}>
+                        key = {opcao.value}
+                        onClick={()=> setOrdenador(opcao.value) }>
                             {opcao.nome}
                         </div>
                     )
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,12 +7,16 @@ import Filtros from "./Filtros";
 import Ordenador from "./Ordenador";
 
 export default function Cardapio(){
+    // pelo principio da responsabilidade única, se usa as const para use state 
     //constantes, pra puxarem valores 
     //de outros arquivos e setarem em estados
     const [busca, setBusca] = useState("");
 
     //Para o caso do filtro, ele tem que começar vazio, logo terá nulo setado
     const [filtro, setFiltro] = useState<number | null>(null);
+
+    //Para ordenador : 
+    const [ordenador, setOrdenador] = useState("");
 
     return(
         <main>
@@ -40,7 +44,9 @@ export default function Cardapio(){
                     setFiltro={setFiltro}
                     />
 
-                    <Ordenador/>
+                    <Ordenador
+                    ordenador={ordenador}
+                    setOrdenador={setOrdenador}/>
                 </div>
             </section>
 
