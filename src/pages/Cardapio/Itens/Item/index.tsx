7a5b2198bdf90b5426a5bea7797f2f1c4cef716c
Rander Felipe Miranda de Paula <rander.paula@estudante.iftm.edu.br>
2023-03-22 10:35:25 -0300

    Organizando imagens e puxando-as dinamicamente para seus respectivos itens

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,6 @@
 import styles from './Item.module.scss';
-import logo from 'assets/logo.svg';
 import cardapio from '../itens.json';
+import classNames from 'classnames';
 
 //Funcao type está importando os valores do json/backend
 type Props = typeof cardapio[0];
@@ -8,19 +8,26 @@ type Props = typeof cardapio[0];
 export default function Item(props: Props){
     //Aqui o const irá pegar os valores obtidos com o props
     //e inserir eles dentro das tags html dinamicamente
-    const {title, description, category, size, serving, price } = props;
+    const {title, description, category, size, serving, price, photo } = props;
 
     return(
         //Exibição de cada dados dos items puxados
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src ='' alt = {title} />
+                <img src ={photo} alt = {title} />
             </div>
             
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}><h2>{title}</h2><p>{description}</p></div>
                 <div className={styles.item__tags}>
-                <div className={styles.item__tipo}>{category.label}</div>
+                    
+                <div className={classNames({
+                //Cada tipo de comida vai ter um scss
+                    [styles.item__tipo]: true,
+                    //Assim ele estiliza diferentemente pra cada um dinamicamente : 
+                    [styles[`item__tipo__${category.label.toLowerCase()}`]] : true
+                })}>{category.label}</div>
+
                 <div className={styles.item__porcao}>{size}g</div>
                 <div className={
                 //Como aqui pode servir uma pessoa só, deve ser conferir para poder apresentar corretamente
