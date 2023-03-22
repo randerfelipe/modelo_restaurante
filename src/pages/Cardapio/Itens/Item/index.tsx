bd913ae6a7087079e734735e43c6383ea565dd83
Rander Felipe Miranda de Paula <rander.paula@estudante.iftm.edu.br>
2023-03-22 10:15:12 -0300

    Populando os itens com os dados puxados do banco/json

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,20 +1,34 @@
 import styles from './Item.module.scss';
 import logo from 'assets/logo.svg';
+import cardapio from '../itens.json';
 
-export default function Item(){
+//Funcao type está importando os valores do json/backend
+type Props = typeof cardapio[0];
+
+export default function Item(props: Props){
+    //Aqui o const irá pegar os valores obtidos com o props
+    //e inserir eles dentro das tags html dinamicamente
+    const {title, description, category, size, serving, price } = props;
+
     return(
+        //Exibição de cada dados dos items puxados
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src ={logo} alt = "imagem" />
+                <img src ='' alt = {title} />
             </div>
-
+            
             <div className={styles.item__descricao}>
-                <div className={styles.item__titulo}><h2> Macarrone</h2><p> descreve macarone</p></div>
+                <div className={styles.item__titulo}><h2>{title}</h2><p>{description}</p></div>
                 <div className={styles.item__tags}>
-                <div className={styles.item__tipo}>Massa</div>
-                <div className={styles.item__porcao}>400g</div>
-                <div className={styles.item__qtdpessoas}>serve 2 pessoas </div>
-                <div className={styles.item__valor}>5 reais</div></div>
+                <div className={styles.item__tipo}>{category.label}</div>
+                <div className={styles.item__porcao}>{size}g</div>
+                <div className={
+                //Como aqui pode servir uma pessoa só, deve ser conferir para poder apresentar corretamente
+                    styles.item__qtdpessoas}>Serve {serving} pessoa
+                {serving ===1 ? "" : 's'} </div>
+                <div className={
+                    //A funcao .toFixed(2) insere o ".00" 
+                    styles.item__valor}>R${price.toFixed(2)}</div></div>
             </div>
         </div>
     )
